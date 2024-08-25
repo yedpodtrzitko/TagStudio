2c01b784e8e0b986703417f5c051faca05b13af4
Quinten Cabo <tintin10q@hotmail.com>
2024-08-25 15:27:49 +0200

    Appease ruff

--- tagstudio/tag_studio.py
+++ tagstudio/tag_studio.py
@@ -9,6 +9,7 @@ import logging
 
 import os
 import sys
+
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__)))
 )  # add this so that `poetry run tagstudio` works, but src on the import path
