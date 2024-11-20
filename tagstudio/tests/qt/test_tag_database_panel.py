07899fcdae53e3e7fc363ba838e4e5ad22eb07f5
yedpodtrzitko <yed@vanyli.net>
2024-11-20 10:55:30 +0700

    add tag panel into left sidebar

---
+++ tagstudio/tests/qt/test_tag_database_panel.py
@@ -0,0 +1,14 @@
+from src.qt.modals.tag_database import TagDatabasePanel
+
+
+def test_panel_tag_search(qt_driver, library):
+    # Given
+    panel = TagDatabasePanel(library, is_popup=False)
+    assert len(panel.scroll_layout.children()) == 0
+
+    # When
+    tag = library.tags[0]
+    panel.update_tags(tag.name)
+
+    # Then
+    assert panel.scroll_layout.count() == 1
